14e79496ebeaa32be4fe69fc85c38a88a12ea7d4
Jacob Patel <jseanpatel@gmail.com>
2020-04-30 00:09:18 -0700

    working video output

--- src/components/video.js
+++ src/components/video.js
@@ -1,41 +1,45 @@
 import React, { useEffect } from "react"
-import axios from 'axios';
+import axios from "axios"
 
 const Video = () => {
-  const { connect } = require("twilio-video")
+  const {
+    connect,
+    createLocalAudioTrack,
+    createLocalVideoTrack,
+  } = require("twilio-video")
 
-  
+  useEffect(() => {}, [])
 
-  useEffect(() => {}, []);
-    
   function call() {
-
     const getParticipantToken = async ({ identity, room }) => {
       // const params = new URLSearchParams();
       const result = await axios({
-        method: 'POST',
-        url: 'http://localhost:8080/token',
+        method: "POST",
+        url: "http://localhost:8080/token",
         data: { identity, room },
-      });
-      return result;
+      })
+      return result
     }
-    getParticipantToken({identity: 'Jacob', room: 'Treetop-Testing'})
-    .then(res => res.data)
-    .then(data => {
-      connect(data, { name: "Treetop-Testing" }).then(
-        room => {
-          console.log(`Successfully joined a Room: ${room}`)
-          room.on("participantConnected", participant => {
-            console.log(`A remote Participant connected: ${participant}`)
-          })
-        },
-        error => {
-          console.error(`Unable to connect to Room: ${error.message}`)
-        }
-      )
-    });
-  
-    // 
+
+    getParticipantToken({ identity: "Jacob", room: "Treetop-Testing" })
+      .then(res => res.data)
+      .then(data => {
+        connect(data, { name: "Treetop-Testing" }).then(
+          room => {
+            console.log(`Successfully joined a Room: ${room}`)
+            createLocalVideoTrack().then(track => {
+              const localMediaContainer = document.getElementById("local-media")
+              localMediaContainer.appendChild(track.attach())
+            })
+            room.on("participantConnected", participant => {
+              console.log(`A remote Participant connected: ${participant}`)
+            })
+          },
+          error => {
+            console.error(`Unable to connect to Room: ${error.message}`)
+          }
+        )
+      })
   }
 
   // function generateCameraPreview() {
@@ -47,14 +51,12 @@ const Video = () => {
   //   })
   // }
 
-
-
+  // need a max height
   return (
-    <div>
+    <div id="local-media">
       <button className="w-full bg-white" onClick={() => call()}>
         click me to test
       </button>
-
     </div>
   )
 }
