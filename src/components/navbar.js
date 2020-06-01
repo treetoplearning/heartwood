605f3ebbb6c6071ff86d0632ca6029066a26a871
Jacob Patel <jseanpatel@gmail.com>
2020-05-31 17:54:48 -0700

    hid elements on learn

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -16,9 +16,7 @@ const Navbar = () => {
   const state = useContext(HeartwoodStateContext)
   const dispatch = useContext(HeartwoodDispatchContext)
 
-  const [photoUrl, setPhotoUrl] = useState(
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-  )
+  const [photoUrl, setPhotoUrl] = useState("https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80")
   const [displayName, setDisplayName] = useState("Treetop Guest")
 
   function logOutAndClearState() {
@@ -135,7 +133,7 @@ const Navbar = () => {
               >
                 <div className="py-1 bg-white rounded-md shadow-xs">
                   <Link
-                    to="/"
+                    to="learn"
                     className="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
                   >
                     Your Profile
--- src/pages/learn.js
+++ src/pages/learn.js
@@ -1,40 +1,55 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect } from "react"
 import Navbar from "../components/navbar"
 import IDE from "../components/ide"
 import Term from "../components/term"
 import Video from "../components/video"
-import { HeartwoodDispatchContext } from "../state/HeartwoodContextProvider"
+import { navigate } from "gatsby"
+import { HeartwoodStateContext, HeartwoodDispatchContext } from "../state/HeartwoodContextProvider"
+import { isLoggedIn } from "../utils/utils"
 
 import "../styles/video.css"
 
 export default () => {
+  const state = useContext(HeartwoodStateContext)
   const dispatch = useContext(HeartwoodDispatchContext)
+
+  useEffect(() => {
+    if (isLoggedIn(state.user)) {
+      console.log(state.user)
+    } else {
+      navigate("/signin")
+    }
+  }, [state.user])
 
   return (
     <div className="flex flex-col w-full h-screen bg-base">
-      <Navbar />
-      <div className="flex flex-row items-center justify-center w-full h-full ">
-        <div className="flex flex-row w-full px-10">
-          <div className="flex flex-col w-1/4">
-            <div className="m-3 bg-black rounded-lg">
-              <div className="relative rounded-lg pb-3/4">
-                <div className="absolute object-cover w-full h-full">
-                  <Video className="rounded-lg" />
+      {isLoggedIn(state.user) && (
+        <>
+          <Navbar />
+          <div className="flex flex-row items-center justify-center w-full h-full ">
+            <div className="flex flex-row w-full px-10">
+              <div className="flex flex-col w-1/4">
+                <div className="m-3 bg-black rounded-lg">
+                  <div className="relative rounded-lg pb-3/4">
+                    <div className="absolute object-cover w-full h-full">
+                      <Video className="rounded-lg" />
+                    </div>
+                  </div>
+                </div>
+                <div className="h-full m-3 bg-blue-300 rounded-lg"></div>
+              </div>
+              <div className="flex flex-col w-3/4">
+                <div className="h-64 m-3 overflow-scroll rounded-lg bg-ideBase">
+                  <IDE />
+                </div>
+                <div className="h-48 px-4 m-3 overflow-x-hidden bg-black rounded-lg">
+                  <Term termId="term" />
                 </div>
               </div>
             </div>
-            <div className="h-full m-3 bg-blue-300 rounded-lg"></div>
           </div>
-          <div className="flex flex-col w-3/4">
-            <div className="h-64 m-3 overflow-scroll rounded-lg bg-ideBase">
-              <IDE />
-            </div>
-            <div className="h-48 px-4 m-3 overflow-x-hidden bg-black rounded-lg">
-              <Term termId="term" />
-            </div>
-          </div>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   )
 }
