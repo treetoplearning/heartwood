a99ba341b8296905679fad560c652dcc1cc95583
Jacob Patel <jseanpatel@gmail.com>
2020-06-27 02:55:11 -0700

    added support for time and tracking date

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -9,18 +9,11 @@ import Navbar from "../components/navbar"
 import { HeartwoodStateContext, HeartwoodDispatchContext } from "../state/HeartwoodContextProvider"
 import { firestore } from "../firebase/firebase"
 
-//  onChange={(event) => onChangeHandler(event)}
-// onClick={(event) => submitForm(event)}
-
 const Schedule = () => {
   const dispatch = useContext(HeartwoodDispatchContext)
   const state = useContext(HeartwoodStateContext)
 
-  const [form, setForm] = useState({ date: "" })
-
-  const setDate = (event) => {
-    setForm({ ...form, date: event.dateStr })
-  }
+  const [form, setForm] = useState({ date: "", time: "13:30" })
 
   useEffect(() => {
     fetch("http://localhost:5000/schedulesetup", { method: "GET" })
@@ -28,8 +21,12 @@ const Schedule = () => {
       .then((res) => {
         let calendarElement = document.getElementById("calendarElement")
 
-        let calendar = new Calendar(calendarElement, {plugins: [dayGridPlugin, googleCalendarPlugin],
+        let calendar = new Calendar(calendarElement, {plugins: [dayGridPlugin, googleCalendarPlugin, interactionPlugin],
           initialView: "dayGridMonth",
+          dateClick: function (info) {
+            setForm({ ...form, date: info.dateStr })
+          },
+          selectable: true,
           googleCalendarApiKey: res.api_key,
           events: { googleCalendarId: res.calendar_id }})
 
@@ -39,20 +36,21 @@ const Schedule = () => {
   return (
     <div className="w-screen min-h-screen bg-base">
       <Navbar />
-      <div className="py-24 font-mono">
+      <div className="py-8 font-mono">
         <div className="w-11/12 px-6 py-8 mx-auto bg-white rounded rounded-xl lg:w-11/12 md:w-3/4 md:px-12">
-          <form className="flex flex-col items-center justify-between mb-10 sm:flex-row">
-            <div className="mb-5">
+          <h1 className="pt-4 mb-2 text-3xl font-bold text-center">Schedule</h1>
+          <form className="flex flex-col items-center justify-between pt-8 mb-10 sm:flex-row">
+            <div className="mb-5 text-center">
               <label htmlFor="lessonDate" className="mr-2 font-semibold text-center">
                 Lesson Date:
               </label>
               <input
                 required
                 type="date"
-                className="w-auto px-6 py-2 leading-tight border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                className="w-auto px-5 py-2 leading-tight border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                 name="lessonDate"
                 value={form.date}
-                onChange={(event) => setDate(event)}
+                onChange={(event) => setForm({ ...form, date: event.value })}
                 id="lessonData"
               />
             </div>
@@ -66,7 +64,7 @@ const Schedule = () => {
                 type="time"
                 className="w-auto px-5 py-2 leading-tight text-center border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                 name="lessonTime"
-                value="13:30"
+                value={form.time}
                 id="lessonTime"
               />
             </div>
