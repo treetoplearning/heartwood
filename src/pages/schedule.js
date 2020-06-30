30ed0482d657c7520d9ca0523ef9ee2725b3711c
Jacob Patel <jseanpatel@gmail.com>
2020-06-29 18:39:11 -0700

    added one click event booking

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -5,130 +5,95 @@ import googleCalendarPlugin from "@fullcalendar/google-calendar"
 import interactionPlugin from "@fullcalendar/interaction"
 
 import Navbar from "../components/navbar"
+import LoadingAnimation from "../components/loadinganimation"
 
 import { HeartwoodStateContext, HeartwoodDispatchContext } from "../state/HeartwoodContextProvider"
 import { firestore } from "../firebase/firebase"
+
+import gear from "../assets/gear.svg"
 
 const Schedule = () => {
   const dispatch = useContext(HeartwoodDispatchContext)
   const state = useContext(HeartwoodStateContext)
 
-  const [form, setForm] = useState({ date: "", time: "13:30", message: { text: "", type: "" } })
+  const [form, setForm] = useState({message: { text: "", type: "" }, isLoading: true })
 
-  const scheduleMeeting = (event) => {
-    event.preventDefault()
-    if (validateInputs()) {
-      fetch("http://localhost:5000/schedulemeeting", {method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({email: state.user.email,
-          date: form.date,
-          time: form.time,
-          uid: state.user.uid,
-          firstName: state.user.firstName,
-          lastName: state.user.lastName})})
-        .then((res) => {
-          setForm({ ...form, message: { text: "Scheduling request was recieved", type: "success" } })
-        })
-        .catch((err) => {
-          setForm({ ...form, message: { text: err, type: "error" } })
-          console.log(err)
-        })
-
-      console.log("schedule the meeting")
-    } else {
-      console.log("inputs are not valid")
-    }
-  }
-
-  const validateInputs = () => {
-    if (form.email === "" || form.time === "") {
-      return false
-    }
-    return true
+  // take an input event and book that lesson to the user in state
+  const bookLesson = (lessonInfo) => {
+    fetch("http://localhost:5000/bookLesson", {method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ uid: state.user.uid, lessonId: lessonInfo.id, email: state.user.email, firstName: state.user.firstName, lastName: state.user.lastName })})
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => console.log("error in booking lesson", err))
   }
 
   useEffect(() => {
-    fetch("http://localhost:5000/schedulesetup", { method: "GET" })
-      .then((res) => res.json())
-      .then((res) => {
-        let calendarElement = document.getElementById("calendarElement")
 
-        let calendar = new Calendar(calendarElement, {plugins: [dayGridPlugin, googleCalendarPlugin, interactionPlugin],
-          initialView: "dayGridMonth",
-          dateClick: function (info) {
-            setForm({ ...form, date: info.dateStr })
-          },
-          weekends: false,
-          selectable: true,
-          googleCalendarApiKey: res.api_key,
-          events: { googleCalendarId: res.calendar_id }})
+    if (state.user) {
+      fetch("http://localhost:5000/getUserEvents", {method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ uid: state.user.uid })})
+        .then((res) => res.json())
+        .then((res) => {
+          setForm({ ...form, isLoading: false })
 
-        calendar.render()
-      })
-  }, [])
+          let calendarElement = document.getElementById("calendarElement")
+
+          let calendar = new Calendar(calendarElement, {plugins: [dayGridPlugin, googleCalendarPlugin, interactionPlugin],
+            initialView: "dayGridMonth",
+            dateClick: function (info) {
+              setForm({ ...form, date: info.dateStr, isLoading: false })
+            },
+            eventClick: function (info) {
+              
+              // ensure that the event is not already booked before booking the lesson
+              if (info.event.extendedProps.booked === false) {
+                bookLesson({id: info.event.id})
+              } else {
+                console.log("the event is already booked")
+              }
+            },
+            weekends: false,
+            header: "",
+            selectable: true,
+            events: res.events})
+          calendar.render()
+        })
+        .catch((err) => console.log("error in fetching events", err))
+    }
+  }, [state.user])
 
   return (
     <div className="w-screen min-h-screen bg-base">
       <Navbar />
-      <div className="py-8 font-mono">
-        <div className="w-11/12 px-6 py-8 mx-auto bg-white rounded rounded-xl lg:w-11/12 md:w-3/4 md:px-12">
-          <h1 className="pt-4 mb-2 text-3xl font-bold text-center">Schedule</h1>
-          {form.message.type === "error" && (
-            <div className="w-full py-4 mb-3 text-center text-white bg-red-600 rounded-lg">{form.message.text}</div>
-          )}
-          {form.message.type === "success" && (
-            <div className="w-full py-4 mb-3 text-center text-white rounded-lg bg-base">{form.message.text}</div>
-          )}
-          <h1 className="w-full py-2 mb-3 text-center ">
-            To schedule a lesson: select a date and time, then click submit.
-          </h1>
-          <form
-            className="flex flex-col items-center justify-between pt-8 mb-10 lg:flex-row"
-            onSubmit={(event) => scheduleMeeting(event)}
-          >
-            <div className="pb-5 text-center">
-              <label htmlFor="lessonDate" className="mr-2 font-semibold text-center">
-                Lesson Date:
-              </label>
-              <input
-                required
-                type="date"
-                className="w-auto px-4 py-2 leading-tight border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                name="lessonDate"
-                value={form.date}
-                onChange={(event) => setForm({ ...form, date: event.target.value })}
-                id="lessonData"
-              />
+      {form.isLoading && (
+        <div className="flex self-center justify-center w-screen h-screen ">
+          {" "}
+          <LoadingAnimation data={gear} />
+        </div>
+      )}
+      {!form.isLoading && (
+        <div className="py-8 font-mono">
+          <div className="w-11/12 px-6 py-8 mx-auto bg-white rounded rounded-xl lg:w-11/12 md:w-3/4 md:px-12">
+            <h1 className="pt-4 mb-2 text-3xl font-bold text-center">Schedule</h1>
+            {form.message.type === "error" && (
+              <div className="w-full py-4 mb-3 text-center text-white bg-red-600 rounded-lg">{form.message.text}</div>
+            )}
+            {form.message.type === "success" && (
+              <div className="w-full py-4 mb-3 text-center text-white rounded-lg bg-base">{form.message.text}</div>
+            )}
+            <h1 className="w-full py-2 mb-3 text-center ">
+              To schedule a lesson: select a date and time, then click submit.
+            </h1>
+            <div className="pb-8">
+              <div id="calendarElement"></div>
             </div>
-            <div className="pb-5 text-center">
-              <label htmlFor="lessonTime" className="mr-2 font-semibold text-center ">
-                Lesson Time:
-              </label>
-              <input
-                required
-                readOnly
-                type="time"
-                className="w-auto px-4 py-2 leading-tight text-center border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                name="lessonTime"
-                value={form.time}
-                id="lessonTime"
-              />
-            </div>
-
-            <div className="pb-5 text-center">
-              <input
-                required
-                type="submit"
-                className="w-auto px-4 py-2 leading-tight text-white transition duration-100 ease-in-out bg-gray-600 border-gray-600 rounded shadow appearance-none hover:opacity-75 focus:outline-none focus:shadow-outline"
-                value="Submit"
-              />
-            </div>
-          </form>
-          <div className="pb-8">
-            <div id="calendarElement"></div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
