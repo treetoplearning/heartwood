5a7908990ba6d175388751bece17b78ae3f529ce
Jacob Patel <jseanpatel@gmail.com>
2020-07-03 01:20:39 -0700

    added conditional navigation on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,58 @@
-import React, { useContext, useEffect } from "react"
-import Navbar from "../components/navbar"
+import React, { useContext, useEffect, useState } from "react"
+import useInterval from "../hooks/useInterval"
+
 import { Link } from "gatsby"
-import { HeartwoodStateContext, HeartwoodDispatchContext } from "../state/HeartwoodContextProvider"
+import { HeartwoodStateContext } from "../state/HeartwoodContextProvider"
 import { isLoggedIn, signUpComplete } from "../utils/utils"
 
+import Navbar from "../components/navbar"
 import CollectInfo from "../components/collectinfo"
+
+import { format, differenceInMinutes } from "date-fns"
 
 const IndexPage = () => {
   const state = useContext(HeartwoodStateContext)
-  const dispatch = useContext(HeartwoodDispatchContext)
+  const [form, setForm] = useState({ startTime: "", stopTime: "", showMeetingButton: false, lessonDescription: "" })
+
+  // every second check
+  useInterval(() => {
+    const currentTime = new Date()
+
+    // Check if the current time is within five minutes either way of the next lesson's time
+    if (differenceInMinutes(form.startTime, currentTime) <= 5 && differenceInMinutes(currentTime, form.stopTime) <= 5) {
+     setForm({...form, showMeetingButton: true})
+    }
+  }, 1000)
 
   useEffect(() => {
     if (state.user) {
-    fetch("http://localhost:5000/getNextMeeting", {method: "POST",
-        headers: { "Content-Type": "application/json"},
-        body: JSON.stringify({ uid: state.user.uid})})
+      fetch("http://localhost:5000/getNextMeeting", {method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ uid: state.user.uid })})
         .then((res) => res.json())
-        .then((res) => console.log(res))
+        .then((res) => {
+
+            
+          
+          if (res.startTime === -1 && res.stopTime === -1) {
+            // catch case where there is not a next meeting
+
+            const formattedStartTime = ""
+            const formattedStopTime = ""
+
+            setForm({ ...form, startTime: formattedStartTime, stopTime: formattedStopTime, lessonDescription: "You do not have any upcoming lessons scheduled" })
+
+          } else {
+            // otherwise handle the case where a next meeting exists
+            const formattedStartTime = new Date(res.startTime.dateTime)
+            const formattedStopTime = new Date(res.stopTime.dateTime)
+
+            const lessonDescription = format(formattedStartTime, "'Your next lesson is on: ' EEEE, LLLL do, y 'at' h:mm a '(PST)'")
+            setForm({ ...form, startTime: formattedStartTime, stopTime: formattedStopTime, lessonDescription: lessonDescription})
+          }
+
+        
+        })
     }
   }, [state.user])
 
@@ -26,12 +62,19 @@ const IndexPage = () => {
         <>
           <Navbar />
 
-          <Link to="learn" className="text-3xl text-center text-white underline ">
+          <div className="flex flex-col">
+
+        
+
+          <div>
+            <p className="mt-10 text-xl text-center text-white"> {form.lessonDescription}</p>
+          </div>
+        {form.showMeetingButton &&
+          <Link to="learn" className="self-center w-1/3 px-4 py-2 mt-10 text-2xl text-center text-black bg-white rounded-xl ">
             {" "}
             Head to your lesson{" "}
           </Link>
-          <div>
-            <p className="text-2xl text-center text-white"> Your next lesson is on </p>
+}
           </div>
         </>
       ) : (
