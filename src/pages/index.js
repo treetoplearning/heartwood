ddc618816bd344244d81aa818bf4dc3ba4029da2
Jacob Patel <jseanpatel@gmail.com>
2020-07-02 22:07:38 -0700

    added /getNextMeeting

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,16 +10,29 @@ const IndexPage = () => {
   const state = useContext(HeartwoodStateContext)
   const dispatch = useContext(HeartwoodDispatchContext)
 
+  useEffect(() => {
+    if (state.user) {
+    fetch("http://localhost:5000/getNextMeeting", {method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify({ uid: state.user.uid})})
+        .then((res) => res.json())
+        .then((res) => console.log(res))
+    }
+  }, [state.user])
+
   return (
-    <div className="flex flex-col w-full h-auto h-screen pb-40 bg-base">
+    <div className="flex flex-col w-full h-auto h-screen pb-40 font-mono bg-base">
       {signUpComplete(state.user) ? (
         <>
           <Navbar />
 
-          <Link to="learn" className="font-mono text-3xl text-center text-white underline ">
+          <Link to="learn" className="text-3xl text-center text-white underline ">
             {" "}
             Head to your lesson{" "}
           </Link>
+          <div>
+            <p className="text-2xl text-center text-white"> Your next lesson is on </p>
+          </div>
         </>
       ) : (
         isLoggedIn(state.user) && <CollectInfo />
